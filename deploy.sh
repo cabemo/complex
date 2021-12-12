95ff419dc3b15af995eae99972dfa7b1d27adcaf
Emilio Cantón <eecanton@gmail.com>
2021-12-12 13:29:07 -0600

    Add deployment config

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build \
+  -t cabemo/multi-client:latest \
+  -t cabemo/multi-client:$SHA \
+  -f ./client/Dockerfile ./client
+docker build \
+  -t cabemo/multi-server:latest \
+  -t cabemo/multi-server:$SHA \
+  -f ./server/Dockerfile ./server
+docker build \
+  -t cabemo/multi-worker:latest \
+  -t cabemo/multi-worker:$SHA \
+  -f ./worker/Dockerfile ./worker
+for image in client server worker; do docker push cabemo/multi-$image; docker push cabemo/multi-$image:$SHA; done
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=cabemo/multi-server:$SHA
+kubectl set image deployments/client-deployment server=cabemo/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=cabemo/multi-worker:$SHA
